d5dcb11f6d54714533a44552d5712eccc732b99f
danubiobwm <danubio.bwm@gmail.com>
2019-09-25 15:36:05 -0300

    Authentication Stylization

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu E-mail" />
+        <input type="password" placeholder="Sua Senha" />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já tenho Login</Link>
+      </form>
+    </>
+  );
 }
