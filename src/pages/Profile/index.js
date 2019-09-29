cda212879a2c7ed52c319b0bc765d4e601eb4ca5
danubiobwm <danubio.bwm@gmail.com>
2019-09-29 19:13:18 -0300

    Profile Page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import {useSelector} from 'react-redux';
+import {Form, Input} from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import {Container} from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" placeholder="Seu E-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha Atual"
+        />
+        <Input type="password" name="Password" placeholder=" Nova Senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+        <button type="submit"> Atualizar Perfil</button>
+      </Form>
+      <button type="button"> Sair do Gobarber </button>
+    </Container>
+  );
 }
